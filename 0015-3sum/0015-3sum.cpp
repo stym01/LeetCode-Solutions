0c31939e3f2df08d736d8abcb5a863cbd2cdffe0
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2025-06-26 12:21:31 +0530

    Time: 55 ms (39.17%), Space: 31 MB (35.47%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) {
-        sort(a.begin(),a.end());
-        
-        vector<vector<int>>ans;
-        int n=a.size();
+    vector<vector<int>> threeSum(vector<int>& nums) {
         int i=0;
-        set<vector<int>>st;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         while(i<n-2){
-            if(i>0 && a[i]==a[i-1]) {i++;
-            continue;}
-            int j=i+1;
-            int k=n-1;
+            if(i>0 && nums[i]==nums[i-1]) {
+                i++;
+                continue;
+            }
+            int j=i+1,k=n-1;
             while(j<k){
-                int x=a[i]+a[j]+a[k];
-                if(x>0)
-                k--;
-                else if(x<0) j++;
+                int sum=nums[i]+nums[j];
+                sum+=nums[k];
+                if(sum<0) j++;
+                else if(sum>0) k--;
                 else{
-                    vector<int>temp={a[i],a[j],a[k]};
+                    vector<int>temp={nums[i],nums[j],nums[k]};
                     ans.push_back(temp);
-                    int last= a[j], last_high_occurrence = a[k];
-                    while(j<k && a[j]==last)
-                    {j++;}
-                    while(j<k && a[k]==last_high_occurrence)
-                    {k--;}
+                    int last_low=nums[j];
+                    int last_high=nums[k];
+                    while(j<k && nums[j]==last_low) j++;
+                    while(j<k && nums[k]==last_high) k--;
                 }
             }
             i++;
         }
         return ans;
     }
+
 };
