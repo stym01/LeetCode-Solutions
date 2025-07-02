1924e5090480710c34b08ec42adb4a4f7798892d
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2025-07-02 08:06:10 +0530

    Time: 14 ms (0.96%), Space: 9.2 MB (68.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,7 +15,21 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return no_of_paths(0,0,m,n,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        dp[m-1][n-1]=1;
+
+        for(int row=m-1;row>=0;row--){
+            for(int col=n-1;col>=0;col--){
+                if(row==m-1 && col==n-1) continue;
+                cout<<dp[row][col]<<endl;
+                int down=dp[row+1][col];
+                int right=dp[row][col+1];
+                dp[row][col]=down+right;
+            }
+        }
+
+        return dp[0][0];
+        // return no_of_paths(0,0,m,n,dp);
     }
 };
