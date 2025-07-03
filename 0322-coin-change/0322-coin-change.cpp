a454065a4e69b90aae49ea38255f4f2d7d90de76
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2025-07-03 13:29:14 +0530

    Time: 55 ms (19.24%), Space: 54.1 MB (8.1%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int minCoins(int ind,int rem,int n,vector<int>&coins,vector<vector<int>>&dp){
+        if(rem<0) return 1e9;
+        if(ind==n) {
+            if(rem==0) return 0;
+            else return 1e9;
+        }
+        if(dp[ind][rem]!=-1) return dp[ind][rem];
+        //pick
+        int pick=1+minCoins(ind,rem-coins[ind],n,coins,dp);
+        int npick=minCoins(ind+1,rem,n,coins,dp);
+        return dp[ind][rem]=min(pick,npick);
+
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int noOfCoins=minCoins(0,amount,n,coins,dp);
+        if(noOfCoins==1e9) return -1;
+        return noOfCoins;
+    }
+};
