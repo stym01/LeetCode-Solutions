762c0cb44ac3c15eb2a52cad0c56589245102d56
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2025-07-12 10:32:04 +0530

    Time: 11 ms (37.83%), Space: 11.6 MB (41.77%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string txt, string pat) {
+        cout<<pat;
+        int k=pat.size();
+        int n=txt.size();
+        int i=0;
+        vector<int>freq(26,0);
+        vector<int>freqW(26,0);
+        
+        for(int l=0;l<k;l++){
+            freqW[pat[l]-'a']++;
+        }
+        vector<int>ans;
+        
+        int cnt=0;
+        int j=0;
+        while(j<n){
+            
+            freq[txt[j]-'a']++;
+            
+            if(j-i+1<k) j++;
+            
+            else if(j-i+1==k){
+                
+                if(freq==freqW){
+                    ans.push_back(i);
+                }
+                
+                freq[txt[i]-'a']--;
+                
+                i++;
+                j++;
+                
+                
+            }
+            
+            
+        }
+        return ans;
+    }
+};
