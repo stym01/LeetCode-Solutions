446bc5266b82174c38733155dc146bc4fd2e8fa5
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2025-07-15 07:57:45 +0530

    Time: 98 ms (7.67%), Space: 31.9 MB (16.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -25,12 +25,12 @@ public:
                 return dp[ind1][ind2]=f(ind1+1,ind2+1,s,p,dp);
             }
             else if(p[ind2]=='*'){
-                // for(int i=ind1-1;i<n1;i++){
-                //     if(f(i+1,ind2+1,s,p)) return true;
-                // } 
-                bool x=f(ind1+1,ind2,s,p,dp);
-                bool y=f(ind1,ind2+1,s,p,dp);
-                return dp[ind1][ind2]=x|y;
+                for(int i=ind1-1;i<n1;i++){
+                    if(f(i+1,ind2+1,s,p,dp)) return dp[ind1][ind2]=true;
+                } 
+                // bool x=f(ind1+1,ind2,s,p,dp);
+                // bool y=f(ind1,ind2+1,s,p,dp);
+                // return dp[ind1][ind2]=x|y;
             }
         }
         return dp[ind1][ind2]=false;
@@ -41,37 +41,37 @@ public:
         int n1=s.size();
         int n2=p.size();
 
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
 
-        dp[n1][n2]=1;
+        // dp[n1][n2]=1;
 
-        for(int i=n2-1;i>=0;i--){
-            if(p[i]!='*') break;
-            dp[n1][i]=1;
-            cout<<i<<" ";
-        }
+        // for(int i=n2-1;i>=0;i--){
+        //     if(p[i]!='*') break;
+        //     dp[n1][i]=1;
+        //     cout<<i<<" ";
+        // }
 
-        for(int ind1=n1-1;ind1>=0;ind1--){
-            for(int ind2=n2-1;ind2>=0;ind2--){
-                if(s[ind1]==p[ind2]){
-                    dp[ind1][ind2]=dp[ind1+1][ind2+1];
-                }
-                else{
-                    if(p[ind2]=='?'){
-                        dp[ind1][ind2]=dp[ind1+1][ind2+1];
-                    }
-                    else if(p[ind2]=='*'){
+        // for(int ind1=n1-1;ind1>=0;ind1--){
+        //     for(int ind2=n2-1;ind2>=0;ind2--){
+        //         if(s[ind1]==p[ind2]){
+        //             dp[ind1][ind2]=dp[ind1+1][ind2+1];
+        //         }
+        //         else{
+        //             if(p[ind2]=='?'){
+        //                 dp[ind1][ind2]=dp[ind1+1][ind2+1];
+        //             }
+        //             else if(p[ind2]=='*'){
 
-                        bool x=dp[ind1+1][ind2];
-                        bool y=dp[ind1][ind2+1];
-                        dp[ind1][ind2]=x|y;
-                    }
-                    else dp[ind1][ind2]=false;
-                }
-            }
-        }
+        //                 bool x=dp[ind1+1][ind2];
+        //                 bool y=dp[ind1][ind2+1];
+        //                 dp[ind1][ind2]=x|y;
+        //             }
+        //             else dp[ind1][ind2]=false;
+        //         }
+        //     }
+        // }
 
-        return dp[0][0];
+        // return dp[0][0];
 
 
 
